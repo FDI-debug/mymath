ad485ba2517528f7080fd3c221ee0b30bff8f5ee
FDI-debug <53896920+FDI-debug@users.noreply.github.com>
2023-10-28 01:31:42 +0300

    Create mymath.go

---
+++ mymath.go
@@ -0,0 +1,9 @@
+// Пример кода для иллюстрации задачи
+package mymath
+
+import "math"
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) int {
+    return math.Sqrt(x)
+}
